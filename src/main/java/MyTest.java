110a35ab3badb132855dedade5c8597cae1e0195
dengbin <bin.deng@moji.com>
2017-04-27 17:34:44 +0800

    'a'

--- src/main/java/MyTest.java
+++ src/main/java/MyTest.java
@@ -1,8 +1,10 @@
-import java.util.Random;
+import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
 
 public class MyTest {
-	public static void main(String[] args) {
-		 byte[] endMark = {-128, -128, -128, -128, -128};
-		 System.out.println(new String(endMark));
+	public static void main(String[] args) throws UnsupportedEncodingException {
+		System.out.println(new String(
+				"\350\257\267\346\261\202\345\271\277\345\221\212\344\275\215\346\270\240\351\201\223\346\227\240\345\271\277\345\221\212"
+						.getBytes("UTF-8")));
 	}
 }
