75f12c75c7450540aa603571bb784d7aba6d7db3
dengbin <dengbin627@sina.com>
2017-06-26 02:41:45 +0100

    test

--- src/main/java/MyTest.java
+++ src/main/java/MyTest.java
@@ -1,43 +1,19 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class MyTest {
 	public static void main(String[] args) {
-		char s = ' ' ;
+		Map a = new HashMap();
+		a.put(1, 1);
+		System.out.println(a);
 		
-		int x = ' ';
-		
-		boolean defined = Character.isDefined(s);
-		System.out.println(defined);
-		boolean identifierIgnorable = Character.isIdentifierIgnorable(s);
-		System.out.println(identifierIgnorable);
-		boolean letterOrDigit = Character.isLetterOrDigit(s);
-		System.out.println(letterOrDigit);
-		boolean spaceChar = Character.isSpaceChar(s);
-		System.out.println(spaceChar);
-		boolean whitespace = Character.isWhitespace(s);
-		System.out.println(whitespace);
-		Character ch = s;
-		System.out.println(ch);
+		Map b = a;
+		System.out.println(a);
+		System.out.println(b);
+		a = new HashMap();
+		System.out.println(a);
+		System.out.println(b);
 		
 	}
-	
-	
-	private static void code(char c){
-		char code;  
-	    code = (char)(c + 1554 % 10);  
-	    System.out.println('4'-48);  
-	    System.out.println(code);  
-	    System.out.println((int)code);  
-	    code = 4;  
-	    System.out.println(0+Character.valueOf(code));  
-	      
-	      
-	    System.out.println((char)(0));  
-	    //强制转换，（取到低16位）4个字节的取低2个字节  
-	    char char1=(char)4;  
-	    System.out.println((char)(c+char1));  
-	      
-	      
-	      
-	    System.out.println(0+Character.valueOf((char)0));  
-	}
-	
+
 }
