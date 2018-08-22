b3febdec1badaa6b7fc87edee8c5e669f2dff9f9
dengbin <bin.deng@moji.com>
2018-08-22 11:45:21 +0800

    new

--- src/main/java/CvsRename.java
+++ src/main/java/CvsRename.java
@@ -1,6 +1,5 @@
 import java.io.File;
 
-
 public class CvsRename {
 
 	 
--- src/main/java/test/java/internet/httpclient/CloseableClient.java
+++ src/main/java/test/java/internet/httpclient/CloseableClient.java
@@ -47,9 +47,12 @@ public class CloseableClient {
     public final static void main(String[] args) throws Exception {
         CloseableHttpClient httpclient = HttpClients.createDefault();
         try {
-            HttpGet httpget = new HttpGet("https://c.gdt.qq.com/gdt_mclick" +
-					".fcg?viewid" +
-					"=fmfg4_WMRuggi8xfziSvr0pIA6mQwyj4GCW7U5tg7gejPvgMIDtVEOVindHbwiyBxYu81cP9ycCNkYOwl0epJpWWS_I_zKrsQMw49vBU0LxTkEfVRMU5BHdEQvoC1Vqap14RkKCYcI3eyy3cVUT2b0WX0!rKitRfq0SzaUbnBVDuvFx1WWpTdxKlooJvvX1qDGQZASQYyNATxFY78HBvoTEfX1vf6VKi&jtype=0&i=1&os=2&asi=%7B%22mf%22%3A%22SM-J7109%22%7D&acttype=1&s=%7B%22req_width%22%3A%22__REQ_WIDTH__%22%2C%22req_height%22%3A%22__REQ_HEIGHT__%22%2C%22width%22%3A%22__WIDTH__%22%2C%22height%22%3A%22__HEIGHT__%22%2C%22down_x%22%3A%22-999%22%2C%22down_y%22%3A%22-999%22%2C%22up_x%22%3A%22-999%22%2C%22up_y%22%3A%22-999%22%7D");
+//        	?id=400000000392&type=2&token=e91de433a10baada0aa94ab2f3b87b95
+            HttpGet httpget = new HttpGet("https://adapi.moji.com/api/misc/stop");
+//            HttpGet httpget = new HttpGet("https://c.gdt.qq.com/gdt_mclick" +
+//					".fcg?viewid" +
+//
+// "=fmfg4_WMRuggi8xfziSvr0pIA6mQwyj4GCW7U5tg7gejPvgMIDtVEOVindHbwiyBxYu81cP9ycCNkYOwl0epJpWWS_I_zKrsQMw49vBU0LxTkEfVRMU5BHdEQvoC1Vqap14RkKCYcI3eyy3cVUT2b0WX0!rKitRfq0SzaUbnBVDuvFx1WWpTdxKlooJvvX1qDGQZASQYyNATxFY78HBvoTEfX1vf6VKi&jtype=0&i=1&os=2&asi=%7B%22mf%22%3A%22SM-J7109%22%7D&acttype=1&s=%7B%22req_width%22%3A%22__REQ_WIDTH__%22%2C%22req_height%22%3A%22__REQ_HEIGHT__%22%2C%22width%22%3A%22__WIDTH__%22%2C%22height%22%3A%22__HEIGHT__%22%2C%22down_x%22%3A%22-999%22%2C%22down_y%22%3A%22-999%22%2C%22up_x%22%3A%22-999%22%2C%22up_y%22%3A%22-999%22%7D");
             System.out.println("Executing request " + httpget.getRequestLine());
             CloseableHttpResponse response = httpclient.execute(httpget);
             try {
