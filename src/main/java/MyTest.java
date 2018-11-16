d0318b2ecaf23c63bef0ad13f4dc7a080c9fd632
dengbin <bin.deng@moji.com>
2018-11-16 16:07:39 +0800

    new

--- src/main/java/MyTest.java
+++ src/main/java/MyTest.java
@@ -1,4 +1,6 @@
 import com.alibaba.fastjson.JSONObject;
+import com.moji.launchserver.entity.City;
+import com.moji.launchserver.util.LoadCityUtil;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
@@ -9,62 +11,60 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class MyTest {
-    static String s = "{\"common\":{\"mnc\":\"11\",\"language\":\"CN\",\"app_version\":\"50070604\"," +
-            "\"idfa_open\":\"1\"," +
-            "\"unix\":\"1535507953383.735\",\"mcc\":\"460\",\"package_name\":\"com.moji.MojiWeather\",\"net\":\"3g\"," +
-            "\"identifier\":\"9FB78921-C665-40E8-8484-B71372E2E4FF\",\"platform\":\"iPhone\"," +
-            "\"token\":\"<1a85d77c79ff0a5b23ee708a1ad1dec265fdb286cc8bf7d99a15a18a1ace9be1>\",\"uid\":652221653," +
-            "\"idfv\":\"475C11B8-50D6-43B4-A5FF-C4889223CB26\",\"height\":2208,\"versionType\":\"1\"," +
-            "\"locationcity\":\"1\",\"width\":1242,\"os_version\":\"8.3\",\"vip\":\"0\",\"pid\":\"9000\"," +
-            "\"device\":\"iPhone7,1\"},\"params\":{\"openType\":false,\"position\":\"POS_GAME_GATE\"," +
-            "\"city_id\":5483," +
-            "\"tabType\":0,\"assistType\":0,\"property_type\":3,\"hotLaunch\":false," +
-            "\"unqid\":\"5c2f4710-abdf-4885-8de0-be4476a4d42b\",\"ad:1,\"weather_index_part\":-1,\"purpose\":3," +
-            "\"ad_partner\":0,\"net\":\"2G\",\"carrier\":3,\"ad_index\":301," +
-            "\"requestId\":\"F2DjUg1CjBnBTgC7xmM69ISATZ9kGky1\",\"uid\":652221653,\"adverting_type\":3," +
-            "\"stat_value\":1," +
-            "\"ad_type\":1,\"materiel_id\":\"5259\",\"ad_id\":10010571,\"price\":\"0\",\"stat_type\"\":\"-\"}}";
+	static String s = "{\"common\":{\"mnc\":\"11\",\"language\":\"CN\",\"app_version\":\"50070604\"," +
+			"\"idfa_open\":\"1\"," +
+			"\"unix\":\"1535507953383.735\",\"mcc\":\"460\",\"package_name\":\"com.moji.MojiWeather\"," +
+			"\"net\":\"3g\"," +
+			"\"identifier\":\"9FB78921-C665-40E8-8484-B71372E2E4FF\",\"platform\":\"iPhone\"," +
+			"\"token\":\"<1a85d77c79ff0a5b23ee708a1ad1dec265fdb286cc8bf7d99a15a18a1ace9be1>\",\"uid\":652221653," +
+			"\"idfv\":\"475C11B8-50D6-43B4-A5FF-C4889223CB26\",\"height\":2208,\"versionType\":\"1\"," +
+			"\"locationcity\":\"1\",\"width\":1242,\"os_version\":\"8.3\",\"vip\":\"0\",\"pid\":\"9000\"," +
+			"\"device\":\"iPhone7,1\"},\"params\":{\"openType\":false,\"position\":\"POS_GAME_GATE\"," +
+			"\"city_id\":5483," +
+			"\"tabType\":0,\"assistType\":0,\"property_type\":3,\"hotLaunch\":false," +
+			"\"unqid\":\"5c2f4710-abdf-4885-8de0-be4476a4d42b\",\"ad:1,\"weather_index_part\":-1,\"purpose\":3," +
+			"\"ad_partner\":0,\"net\":\"2G\",\"carrier\":3,\"ad_index\":301," +
+			"\"requestId\":\"F2DjUg1CjBnBTgC7xmM69ISATZ9kGky1\",\"uid\":652221653,\"adverting_type\":3," +
+			"\"stat_value\":1," +
+			"\"ad_type\":1,\"materiel_id\":\"5259\",\"ad_id\":10010571,\"price\":\"0\",\"stat_type\"\":\"-\"}}";
 
-    static String data = "{\"adclick_log_ali932\":\"2018-08-29 11:38:45  - {'common':{'identifier':'861980032814049'," +
-            "'app_version':'1006000002','os_version':'23','device':'RedmiNote3','platform':'Android','pid':'5599'," +
-            "'language':'CN','uid':'332719865','width':1080,'height':1920,'sid':'','snsid':''}," +
-            "'params':{'ad_id':10013997,'ad_index':308,'ad_index_part':-1,'ad_partner':0,'ad_type':1," +
-            "'adverting_type':3,'assistType':0,'carrier':1,'city_id':5339,'materiel_id':'7291','net':'wifi'," +
-            "'openType':false,'position':'POS_WEATHER_HOME_INDEX_ENTRY','price':'0','property_type':1,'purpose':1," +
-            "'requestId':'1535513924092308','stat_type':1,'stat_value':1,'tabType':0,'uid':332719865," +
-            "'unqid':'ef1e891e-4810-495c-86a0-b7a7cc4fa72c','weather':'-'}}\"}";
+	static String data = "{\"adclick_log_ali932\":\"2018-08-29 11:38:45  - " +
+			"{'common':{'identifier':'861980032814049'," +
+			"'app_version':'1006000002','os_version':'23','device':'RedmiNote3','platform':'Android','pid':'5599'," +
+			"'language':'CN','uid':'332719865','width':1080,'height':1920,'sid':'','snsid':''}," +
+			"'params':{'ad_id':10013997,'ad_index':308,'ad_index_part':-1,'ad_partner':0,'ad_type':1," +
+			"'adverting_type':3,'assistType':0,'carrier':1,'city_id':5339,'materiel_id':'7291','net':'wifi'," +
+			"'openType':false,'position':'POS_WEATHER_HOME_INDEX_ENTRY','price':'0','property_type':1,'purpose':1," +
+			"'requestId':'1535513924092308','stat_type':1,'stat_value':1,'tabType':0,'uid':332719865," +
+			"'unqid':'ef1e891e-4810-495c-86a0-b7a7cc4fa72c','weather':'-'}}\"}";
 
-    private static String splitFilter = "  - ";
+	private static String splitFilter = "  - ";
 
-    public static void main(String[] args) throws Exception {
-        BigDecimal price = new BigDecimal("1");
-        BigDecimal priceConvertTemp = price;
-
-        priceConvertTemp = priceConvertTemp.subtract(new BigDecimal("0.5"));
-        System.out.println(price);
-        System.out.println(priceConvertTemp);
-
-    }
+	public static void main(String[] args) throws Exception {
+		LoadCityUtil.init();
+		City provinceByCity = LoadCityUtil.getProvinceByCity(601);
+		System.out.println(provinceByCity);
+	}
 
 
-    private static String MD5(String s) {
-        try {
-            MessageDigest md = MessageDigest.getInstance("MD5");
-            byte[] bytes = md.digest(s.getBytes("utf-8"));
-            return toHex(bytes);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-    }
+	private static String MD5(String s) {
+		try {
+			MessageDigest md = MessageDigest.getInstance("MD5");
+			byte[] bytes = md.digest(s.getBytes("utf-8"));
+			return toHex(bytes);
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
 
-    private static String toHex(byte[] bytes) {
+	private static String toHex(byte[] bytes) {
 
-        final char[] HEX_DIGITS = "0123456789ABCDEF".toCharArray();
-        StringBuilder ret = new StringBuilder(bytes.length * 2);
-        for (int i = 0; i < bytes.length; i++) {
-            ret.append(HEX_DIGITS[(bytes[i] >> 4) & 0x0f]);
-            ret.append(HEX_DIGITS[bytes[i] & 0x0f]);
-        }
-        return ret.toString();
-    }
+		final char[] HEX_DIGITS = "0123456789ABCDEF".toCharArray();
+		StringBuilder ret = new StringBuilder(bytes.length * 2);
+		for (int i = 0; i < bytes.length; i++) {
+			ret.append(HEX_DIGITS[(bytes[i] >> 4) & 0x0f]);
+			ret.append(HEX_DIGITS[bytes[i] & 0x0f]);
+		}
+		return ret.toString();
+	}
 }
--- src/main/java/test/kafka/KafkaProducerTest.java
+++ src/main/java/test/kafka/KafkaProducerTest.java
@@ -5,70 +5,131 @@ import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerRecord;
-import org.apache.kafka.common.PartitionInfo;
-import org.apache.log4j.Logger;
 
-import java.util.List;
+import java.text.ParseException;
 import java.util.Properties;
+import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
 public class KafkaProducerTest {
-    static String s = " - {'common': {},'params': {'city_id': %s,'materiel_id':'0',"
-            + "'ad_index': 306, 'adverting_type':2,'stat_value': 1," +
-            "'ad_id': 400000000363,'price': '1.0','stat_type': 2}}";
-    static HttpClient httpClient = new DefaultHttpClient();
+	static String s = " - {'common': {},'params': {'city_id': 0,'materiel_id':'0',"
+			+ "'ad_index': 306, 'adverting_type':2,'stat_value': 1," +
+			"'ad_id': 400000000363,'price': '1.0','stat_type': 2}}";
+	static HttpClient httpClient = new DefaultHttpClient();
 
-    public static void main(String[] args) {
-        sendkafka();
-//        sendNginx();
-    }
+	public static void main(String[] args) throws ParseException {
+//        sendkafka();
 
+		randomDate();
 
-    private static void sendkafka() {
-        Properties props = new Properties();
+	}
 
-        props.put("bootstrap.servers", "192.168.1.17:9092,192.168.1.17:9093,192.168.1.17:9094");
-        props.put("acks", "all");
-        props.put("retries", 3);
-        props.put("batch.size", 10);
-        props.put("linger.ms", 1000);
-        props.put("buffer.memory", 33554432);
-        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-        //生产者发送消息
-        String topic = "moji_ad_stream26";
-        int  count = 0;
-        Producer<String, String> producer = new KafkaProducer<>(props);
-        for (int hour = 0; hour <23; hour++) {
-            String hourStr;
-            if (hour < 10) {
-                hourStr = "0" + hour;
-            } else {
-                hourStr = String.valueOf(hour);
-            }
+	private static void sendkafka() {
+		Properties props = new Properties();
+
+		props.put("bootstrap.servers", "192.168.1.17:9092,192.168.1.17:9093,192.168.1.17:9094");
+		props.put("acks", "all");
+		props.put("retries", 3);
+		props.put("batch.size", 10);
+		props.put("linger.ms", 1000);
+		props.put("buffer.memory", 33554432);
+		props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+		props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+		//生产者发送消息
+		String topic = "moji_ad_stream26";
+		int count = 0;
+		Producer<String, String> producer = new KafkaProducer<>(props);
+		for (int hour = 0; hour < 23; hour++) {
+			String hourStr;
+			if (hour < 10) {
+				hourStr = "0" + hour;
+			} else {
+				hourStr = String.valueOf(hour);
+			}
 //            2018-06-06 10:19:12
-            for (int minute = 0; minute <60; minute++) {
-                String value;
-                if (minute < 10) {
-                    value = "2018-11-07 " + hourStr + ":0" + minute + ":12 " + String.format(s, hour);
-                } else {
-                    value = "2018-11-07 " + hourStr + ":" + minute + ":12 " + String.format(s, hour+1);
-                }
+			for (int minute = 0; minute < 60; minute++) {
+				String value;
+				if (minute < 10) {
+					value = "2018-11-07 " + hourStr + ":0" + minute + ":12 " + String.format(s);
+				} else {
+					value = "2018-11-07 " + hourStr + ":" + minute + ":12 " + String.format(s);
+				}
 //                Logger.getLogger(KafkaProducerTest.class).info(value);
-                ProducerRecord<String, String> msg = new ProducerRecord<>(topic, value);
-                producer.send(msg);
-                count ++;
-            }
-        }
-        //列出topic的相关信息
+				ProducerRecord<String, String> msg = new ProducerRecord<>(topic, value);
+				producer.send(msg);
+				count++;
+			}
+		}
+		//列出topic的相关信息
 //        List<PartitionInfo> partitions = producer.partitionsFor(topic);
 //        for (PartitionInfo p : partitions) {
 //            System.out.println(p);
 //        }
 
-        System.out.println("send message over." + count);
-        producer.flush();
-        producer.close(100, TimeUnit.MILLISECONDS);
-    }
+		System.out.println("send message over." + count);
+		producer.flush();
+		producer.close(100, TimeUnit.MILLISECONDS);
+	}
+
+
+	private static void randomDate() throws ParseException {
+		Properties props = new Properties();
+
+		props.put("bootstrap.servers", "192.168.1.17:9092,192.168.1.17:9093,192.168.1.17:9094");
+		props.put("acks", "all");
+		props.put("retries", 3);
+		props.put("batch.size", 10);
+		props.put("linger.ms", 1000);
+		props.put("buffer.memory", 33554432);
+		props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+		props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+		//生产者发送消息
+		String topic = "moji_ad_stream26";
+		int count = 0;
+		Producer<String, String> producer = new KafkaProducer<>(props);
+
+		Random minuteRandom = new Random();
+		Random dateRandom = new Random();
+
+		for (int i = 0; i < 100; i++) {
+			int minute = minuteRandom.nextInt(60);
+			int date = dateRandom.nextInt(5) + 1;
+
+			String dateStr ;
+			if (date < 10) {
+				dateStr = "0" + date;
+			} else {
+				dateStr = date +"";
+			}
+
+			int hour = 0;
+			String hourStr;
+			if (hour < 10) {
+				hourStr = "0" + hour;
+			} else {
+				hourStr = String.valueOf(hour);
+			}
+			String value;
+
+			if (minute < 10) {
+				value = "2018-11-" + dateStr + " " + hourStr + ":0" + minute + ":12 " + String.format(s);
+			} else {
+				value = "2018-11-" + dateStr + " " + hourStr + ":" + minute + ":12 " + String.format(s);
+			}
+			System.out.println(value);
+			ProducerRecord<String, String> msg = new ProducerRecord<>(topic, value);
+			producer.send(msg);
+			count++;
+		}
+		//列出topic的相关信息
+//        List<PartitionInfo> partitions = producer.partitionsFor(topic);
+//        for (PartitionInfo p : partitions) {
+//            System.out.println(p);
+//        }
+
+		System.out.println("send message over." + count);
+		producer.flush();
+		producer.close(100, TimeUnit.MILLISECONDS);
+	}
 
 }
